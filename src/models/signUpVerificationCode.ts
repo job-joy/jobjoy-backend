1b464d878ce51172dfd8ffb6cef30958fa3463d6
mersad <mersad_998@yahoo.com>
2021-11-12 21:11:36 +0330

    feat(signUpVerificationCode.ts): set up scheme and model

---
+++ src/models/signUpVerificationCode.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const signUpVerificationCodeSchema = new mongoose.Schema({
+  mobileNumber: {
+    type: String,
+    required: true,
+    minlength: 11,
+    maxlength: 11,
+    unique: true,
+  },
+  requestDate: {
+    type: Date,
+    required: true,
+  },
+  verificationCode: {
+    type: Number,
+    required: true,
+    minlength: 5,
+    maxlength: 5,
+  },
+});
+
+const signUpVerificationCode = mongoose.model(
+  'signUpVerificationCode',
+  signUpVerificationCodeSchema,
+);
+
+export { signUpVerificationCodeSchema, signUpVerificationCode };
