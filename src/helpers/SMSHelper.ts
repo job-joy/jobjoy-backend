f61334e3537df04027d54677621df6e798efdce5
mersad <mersad_998@yahoo.com>
2021-11-14 22:13:46 +0330

    feat: add jsDoc

--- src/helpers/SMSHelper.ts
+++ src/helpers/SMSHelper.ts
@@ -1,5 +1,10 @@
 import kavenegar from 'kavenegar';
 
+/**
+ * send sms with kavenegar API
+ * @param {string} message 
+ * @param {string} mobileNumber 
+ */
 export const sendSMS = (message, mobileNumber) => {
   const SMSserver = kavenegar.KavenegarApi({ apikey: process.env.SMS_TOKEN });
 
