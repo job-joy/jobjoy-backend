c7f431e102038a75c8330501940e89b0587dd13e
mersad <mersad_998@yahoo.com>
2021-11-21 22:44:01 +0330

    feat(feeds.ts): api

---
+++ src/routes/feeds.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { feed } from '../models/feed';
+
+var router = express.Router();
+
+router.post('/createPost', async (req, res) => {
+  const { title, description, images, labels } = req.body;
+
+  res.send({
+    message: 'create post',
+  });
+});
+
+export default router;
