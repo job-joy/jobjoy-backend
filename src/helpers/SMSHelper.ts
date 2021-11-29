2a8b2058b51d95ee59582cec9a64cc309f4628f9
mersad <mersad_998@yahoo.com>
2021-11-29 23:57:05 +0330

    cleanup and declare types

--- src/helpers/SMSHelper.ts
+++ src/helpers/SMSHelper.ts
@@ -3,11 +3,11 @@ import kavenegar from 'kavenegar';
 /**
  * send sms with kavenegar API
  * @function sendSMS
- * @param {string} message 
- * @param {string} mobileNumber 
+ * @param {string} message
+ * @param {string} mobileNumber
  * @returns {void}
  */
-export const sendSMS = (message, mobileNumber) => {
+export const sendSMS = (message: string, mobileNumber: string): void => {
   const SMSserver = kavenegar.KavenegarApi({ apikey: process.env.SMS_TOKEN });
 
   SMSserver.Send({
