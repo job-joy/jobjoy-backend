2e470eb83ef3609e8e817402215a5a077e34286f
mersad <mersad_998@yahoo.com>
2021-11-30 22:29:41 +0330

    feat(label.ts): complete schema

--- src/models/label.ts
+++ src/models/label.ts
@@ -1,7 +1,17 @@
 import mongoose from 'mongoose';
 
 const labelSchema = new mongoose.Schema({
-
+  title: {
+    type: String,
+    required: true,
+  },
+  color:{
+    type: Date,
+    required: true,
+  },
+  icon:{
+    type: Date,
+  },
 });
 
 const label = mongoose.model('label', labelSchema);
